3dd720efeb2dca2dea671c8d0ae501f93511865f
Katherine Beame <katherine.beame@gmail.com>
2016-01-11 13:26:11 -0800

    connected app.jstto index.html

--- js/app.js
+++ js/app.js
@@ -4,4 +4,7 @@ var pikePlace {
   maxCustPerHour: 33,
   avgCupPerCust: 1.2,
   avgPndPerCust: 3.7
-}
+};
+
+var randomCustPikePlace = this.generateRandom(14, 33);
+var beanTotalPerHour = (randomCustPikePlace*avgCupPerCust)+(randomCustPikePlace*avgPndPerCust);
