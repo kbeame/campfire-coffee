e2534f133b5013c89eedf14f837f316a1d61ff22
Katherine Beame <katherine.beame@gmail.com>
2016-01-11 13:05:52 -0800

    Added the object pikePlace to apps.js

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 This is my README FILE
 Author: Katherine Beame
 Began: January 11th 2016
+ Campfire Coffee Puzzle
--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 // my js will be in here
+var pikePlace {
+  minCustPerHour: 14,
+  maxCustPerHour: 33,
+  avgCupPerCust: 1.2,
+  avgPndPerCust: 3.7
+}
