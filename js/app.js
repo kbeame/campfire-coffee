314e22abffc493c4b3cd847c564ec2adbf4c2fee
Katherine Beame <katherine.beame@gmail.com>
2016-01-11 17:04:02 -0800

    Prints Pike Place Data in a list to the web page

--- js/app.js
+++ js/app.js
@@ -1,46 +1,44 @@
-// my js will be in here
-// var hourlyBeanTotal = function () {
-//   console.log(pikePlace.beanTotalPerHour());
-
-
-//attempt # 2
-
-var generateRandom = function(min, max) {
-  return(Math.floor(Math.random() * (max - min + 1)) + min);
-}
-var pikeRandomNum = generateRandom(14, 33);
+// var generateRandom = function(min, max) {
+//   return(Math.floor(Math.random() * (max - min + 1)) + min);
+// }
+// var pikeRandomNum: generateRandom(14, 33);
 
 var pikePlace = {
   minCustPerHour: 14,
   maxCustPerHour: 33,
   avgCupPerCust: 1.2,
   avgPndPerCust: 3.7,
-
+  pikeRandomNum: function() {
+    return(Math.floor(Math.random() * (33 - 14 + 1)) + 14);
+    },
   cupPerHour: function() {
-    return(pikeRandomNum * pikePlace.avgCupPerCust);
+    return(pikePlace.pikeRandomNum() * pikePlace.avgCupPerCust);
     },
   pndPerHour: function() {
-    return(pikeRandomNum * pikePlace.avgPndPerCust);
+    return(pikePlace.pikeRandomNum() * pikePlace.avgPndPerCust);
     },
   beanTotalPerHour: function () {
     return(Math.floor((0.05 * pikePlace.cupPerHour()) + (pikePlace.pndPerHour())));
     }
 }
-// pikePlace.beanTotalPerHour();
-// document.write(pikePlace.beanTotalPerHour());
 
 var hours = ["6:00 am: ", "7:00 am: ", "8:00 am: ", "9:00 am: ", "10:00 am: ", "11:00 am: ",
 "12 noon: ", "1:00 pm: ", "2:00 pm: ", "3:00 pm: ", "4:00 pm: ", "5:00 pm: ", "6:00 pm: ",
 "7:00 pm: ", "8:00 pm: "];
 
-var Show = function() {
-  if () {
+
+
+var show = function() {
   for (var i = 0; i < hours.length; i++) {
-    return((hours[i]) + pikeRandomNum + " customers");
+    if (i < hours.length) {
+      var listEl = document.createElement('ul');
+      listEl.textContent = (hours[i] + pikePlace.pikeRandomNum() + " customers, " + pikePlace.cupPerHour() + " cups, " + pikePlace.pndPerHour() + " pnds to-go");
+      document.body.appendChild(listEl);
+      }
+    else {
+      console.log("done");
+      }
     }
   }
-}
 
-var listEl = document.createElement('ul');  //creating a varibale that is an html element
-listEl.textContent = Show();  //give that varaible some content
-document.body.appendChild(listEl);;      //take that content and put it in the document
+show();
