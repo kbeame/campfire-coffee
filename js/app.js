da886b6fc09b58e03674e9e015acbdc61c3b35a2
Katherine Beame <katherine.beame@gmail.com>
2016-01-11 16:27:19 -0800

    Attempting to print the pertinant info for Pike Place

--- js/app.js
+++ js/app.js
@@ -1,24 +1,7 @@
 // my js will be in here
 // var hourlyBeanTotal = function () {
 //   console.log(pikePlace.beanTotalPerHour());
-// }
-//
-// var generateRandom = function(min, max) {
-//   console.log(Math.floor(Math.random() * (max - min + 1)) + min);
-// }
-//
-// var pikePlace = {
-//   minCustPerHour: 14,
-//   maxCustPerHour: 33,
-//   avgCupPerCust: 1.2,
-//   avgPndPerCust: 3.7,
-//   randomCustNumbPerHour: function() {
-//   console.log(generateRandom(pikePlace.minCustPerHour,pikePlace.maxCustPerHour));
-//   },
-//   beanTotalPerHour: function () {
-//     console.log(Math.floor((pikePlace.randomCustNumbPerHour*pikePlace.avgCupPerCust) + (pikePlace.randomCustNumbPerHour*pikePlace.avgPndPerCust)));
-// }
-// }
+
 
 //attempt # 2
 
@@ -43,6 +26,21 @@ var pikePlace = {
     return(Math.floor((0.05 * pikePlace.cupPerHour()) + (pikePlace.pndPerHour())));
     }
 }
-
 // pikePlace.beanTotalPerHour();
-document.write(pikePlace.beanTotalPerHour());
+// document.write(pikePlace.beanTotalPerHour());
+
+var hours = ["6:00 am: ", "7:00 am: ", "8:00 am: ", "9:00 am: ", "10:00 am: ", "11:00 am: ",
+"12 noon: ", "1:00 pm: ", "2:00 pm: ", "3:00 pm: ", "4:00 pm: ", "5:00 pm: ", "6:00 pm: ",
+"7:00 pm: ", "8:00 pm: "];
+
+var Show = function() {
+  if () {
+  for (var i = 0; i < hours.length; i++) {
+    return((hours[i]) + pikeRandomNum + " customers");
+    }
+  }
+}
+
+var listEl = document.createElement('ul');  //creating a varibale that is an html element
+listEl.textContent = Show();  //give that varaible some content
+document.body.appendChild(listEl);;      //take that content and put it in the document
