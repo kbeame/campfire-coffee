efa11d0ae69e3075515d29c0866c98717388b46c
Katherine Beame <katherine.beame@gmail.com>
2016-01-13 14:46:10 -0800

    I have now seperated out the header full of hours from the constructor function. I have also seperated out the creation of the objects into a seperate function

--- js/app.js
+++ js/app.js
@@ -5,6 +5,33 @@ var hours = ["6:00 am: ", "7:00 am: ", "8:00 am: ", "9:00 am: ", "10:00 am: ", "
 "7:00 pm: ", "8:00 pm: "];
 var coffeeName = ['Pike Place', 'Capitol Hill', 'Seattle Public Library','South Lake Union','SeaTac Airport','Website Sales'];
 
+var allShops = [
+  ['Pike Place', 14, 55, 1.2, 3.7],
+  ['Capitol Hill',32,48,3.2,0.4],
+  ['Seattle Public Library', 49, 75, 2.6, 0.2],
+  ['South Lake Union', 35, 88, 1.3, 3.7,],
+  ['Sea-Tac Airpot', 68, 124, 1.1, 2.7],
+  ['Website Sales', 3, 6, 0, 6.7]]
+
+  var allShopsCollection = [];
+
+  var printShops = function () {
+    var sectEl = document.getElementById('myTable');
+    var table = document.createElement('table');
+    table.id = 'tableid';
+    var trEl = document.createElement('tr')
+    var tHeadEl = document.createElement('th');
+    tHeadEl.textContent = 'Hours';
+    trEl.appendChild(tHeadEl);
+    for (var i = 0; i < hours.length; i++) {
+      var thEl = document.createElement('th');
+      thEl.textContent = hours[i];
+      trEl.appendChild(thEl);
+    }
+    table.appendChild(trEl);
+  }
+  
+  printShops();
 var LocationCoffee = function (locationName, minCust, maxCust, avgCup, avgPnd) {
   this.locationName = locationName;
   this.minCust = minCust;
@@ -16,7 +43,6 @@ var LocationCoffee = function (locationName, minCust, maxCust, avgCup, avgPnd) {
   this.pndHourly = [];
   this.beansTotalHourly = [];
   this.locationPndsTotal = 0;
-  this.coffeeName = ['Pike Place', 'Capitol Hill', 'Seattle Public Library','South Lake Union','SeaTac Airport','Website Sales'];
   this.randomNum = function() {
     for (var i = 0; i < hours.length; i++) {
     this.custHourly.push(Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust);
@@ -55,64 +81,58 @@ var LocationCoffee = function (locationName, minCust, maxCust, avgCup, avgPnd) {
     },
 
   this.renderTable = function() {
-    var sectEl = document.getElementById('myTable');
-    var table = document.createElement('table');
-    var trEl = document.createElement('tr')
-    var tHeadEl = document.createElement('th');
-    tHeadEl.textContent = 'Hours';
-    trEl.appendChild(tHeadEl);
-    for (var i = 0; i < hours.length; i++) {
-      var tdEl = document.createElement('td');
-      tdEl.textContent = hours[i];
-      tHeadEl.appendChild(tdEl);
-    }
-    table.appendChild(tHeadEl);
-    // sectEl.appendChild(tHeadEl);
-//PikePlace RenderTable
-for (var i = 0; i < this.coffeeName.length; i++) {
-  var tBody = document.createElement('tb');
+    this.daily();
+    // var sectEl = document.getElementById('myTable');
+    // var table = document.createElement('table');
+    // var trEl = document.createElement('tr')
+    // var tHeadEl = document.createElement('th');
+    // tHeadEl.textContent = 'Hours';
+    // trEl.appendChild(tHeadEl);
+    // for (var i = 0; i < hours.length; i++) {
+    //   var thEl = document.createElement('th');
+    //   thEl.textContent = hours[i];
+    //   trEl.appendChild(thEl);
+    // }
+    // table.appendChild(trEl);
+  var table = document.getElementById('tableid');
+  var tBody = document.createElement('tbody');
   table.appendChild(tBody);
   var trEl2 = document.createElement('tr');
-  trEl2.textContent = this.coffeeName[i];
+  trEl2.textContent = this.locationName;
   tBody.appendChild(trEl2);
     for (var x = 0; x < this.beansTotalHourly.length; x++) {
       var tdEl2 = document.createElement('td');
       tdEl2.textContent = this.beansTotalHourly[x];
       trEl2.appendChild(tdEl2);
-
-    }
       }
+  // }
       sectEl.appendChild(table);
-  }
-  // sectEl.appendChild(tblEL);
+}
 }
 
 
-var pikePlace = new LocationCoffee('Pike Place', 14, 55, 1.2, 3.7);
-pikePlace.render();
-pikePlace.renderTable();
+var allMustDie = function () {
+  for (var i = 0; i < allShops.length; i++) {
+    allShopsCollection.push(new LocationCoffee(allShops[i][0],allShops[i][1],allShops[i][2],allShops[i][3],allShops[i][4]));
+    allShopsCollection[i].renderTable();
+    allShopsCollection[i].render();
+  }
+}
+allMustDie();
+// var pikePlace = new LocationCoffee('Pike Place', 14, 55, 1.2, 3.7);
+// pikePlace.renderTable();
+// //
+// var capHill = new LocationCoffee('Capitol Hill', 32, 48, 3.2, 0.4);
+// capHill.renderTable();
 //
-var capHill = new LocationCoffee('Capitol Hill', 32, 48, 3.2, 0.4);
-capHill.render();
-capHill.renderTable();
-
-var seaPubLib = new LocationCoffee('Seattle Public Library', 49, 75, 2.6, 0.2);
-seaPubLib.render();
-seaPubLib.renderTable();
-
-var sLakeUnion = new LocationCoffee('South Lake Union', 35, 88, 1.3, 3.7);
-sLakeUnion.render();
-sLakeUnion.renderTable();
-
-var sTacAir = new LocationCoffee('Sea-Tac Airpot', 68, 124, 1.1, 2.7);
-sTacAir.render();
-sTacAir.renderTable();
-
-var websiteSales = new LocationCoffee('Website Sales', 3, 6, 0, 6.7);
-websiteSales.render();
-websiteSales.renderTable();
-
-
-
-
-// Call the renderMonths function
+// var seaPubLib = new LocationCoffee('Seattle Public Library', 49, 75, 2.6, 0.2);
+// // seaPubLib.renderTable();
+//
+// var sLakeUnion = new LocationCoffee('South Lake Union', 35, 88, 1.3, 3.7);
+// // sLakeUnion.renderTable();
+//
+// var sTacAir = new LocationCoffee('Sea-Tac Airpot', 68, 124, 1.1, 2.7);
+// // sTacAir.renderTable();
+//
+// var websiteSales = new LocationCoffee('Website Sales', 3, 6, 0, 6.7);
+// // websiteSales.renderTable();
